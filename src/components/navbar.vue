7f01ec160cf2cb55f4bc3d8f12449fc639205fa0
Edgar Joel EJSL. Sanchez LAscano (Pasante 2023) <esanchez@eeasa.com>
2023-07-13 09:58:33 -0500

    Dar estilo a la caebera

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -51,15 +51,15 @@
 
 <style scoped>
 .header {
-  background: rgba(12, 11, 9, 0.6);
+  background: rgba(46, 139, 87);
   border-bottom: 1px solid rgba(12, 11, 9, 0.6);
   transition: all 0.5s;
 }
 
 .header.header-scrolled {
   top: 0;
-  background: rgba(0, 0, 0, 0.85);
-  border-bottom: 1px solid #37332a;
+  background: rgba(190, 255, 171, 0.644);
+  border-bottom: 1px solid #006400;
 }
 
 .logo-image {

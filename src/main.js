3e3638c3695c6f53f7ae6c06c49938a04c0bbb6b
Edgar <esanchez1169@uta.edu.ec>
2023-07-12 21:42:06 -0500

    Inatalcion de las diferentes librerias de estilo

--- src/main.js
+++ src/main.js
@@ -2,4 +2,24 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from "./routes/routes"
+
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
+
+import VueWriter from "vue-writer"
+
+import { OhVueIcon, addIcons } from "oh-vue-icons"
+
+import { FiHk,FaFlag, RiZhihuFill, MdDesignservicesRound, SiSqlite, SiAdobephotoshop,SiPhp,LaLanguageSolid, GiCommercialAirplane, BiCamera2, GiRallyTheTroops,GiSoccerBall,SiAntdesign, SiYoutubegaming, RiMusic2Fill, PxMenu, CoGithub, BiLinkedin, BiWhatsapp, CoInstagram,BiFacebook, HiDownload, FaFirefoxBrowser,FaVrCardboard, SiStrapi, FaPhoneAlt,HiSolidMail,MdLocationon              } from "oh-vue-icons/icons";
+
+addIcons(FaFlag, RiZhihuFill,FiHk, MdDesignservicesRound, SiSqlite, SiAdobephotoshop,SiPhp,LaLanguageSolid,GiCommercialAirplane, BiCamera2,GiRallyTheTroops, GiSoccerBall,SiAntdesign, SiYoutubegaming,RiMusic2Fill,PxMenu, CoGithub,BiLinkedin, BiWhatsapp, CoInstagram,BiFacebook,HiDownload, FaFirefoxBrowser,FaVrCardboard, SiStrapi, FaPhoneAlt,HiSolidMail,MdLocationon          );
+
+
+
+
+createApp(App)
+.component("v-icon", OhVueIcon)
+.use(router)
+.use(VueWriter)
+.mount('#app')
